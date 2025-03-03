4afdfe8d0f6a4e9c12608681ad2d9c8a2e50a90f
robby209 <robbyhermansyah20@gmail.com>
2025-03-03 10:08:13 +0700

    u1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',     // file bawaan
+                'resources/js/app.js',       // file JS bawaan
+                'resources/css/admin.css',   
+                'resources/css/login.css',   
+                'resources/css/register.css',
+                'resources/css/user.css',    
+            ],
             refresh: true,
         }),
     ],
